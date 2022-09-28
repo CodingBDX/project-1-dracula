f620e5abdae1b828696e69a35bbc9c1ba23d958e
BONZOM Julien <bonzomj@gmail.com>
2022-09-28 14:06:39 +0200

    Fichier initial

--- formations.php
+++ formations.php
@@ -1,7 +1,56 @@
+<?php
+$studies = [
+
+    [
+    'Diplome'=>'Baccalaureat série ES', 
+    'Lieux'=>'Bucarest', 
+    'Date'=> '1897',
+    ],
+    [
+    'Diplome'=>'CAP petite enfance',
+    'Lieux'=>'Timiçoara', 
+    'Date'=> '1623'
+    ],
+    [
+    'Diplome'=>'Doctora de Medecin légiste',
+    'Lieux'=>'Paris',
+    'Date'=> '1940'
+    ],
+    [
+    'Diplome'=>'BTS  Hostellerie restauration',
+    'Lieux'=>'Transilvanie',
+    'Date'=> '1900',
+    ],
+    [
+    'Diplome'=>'Deuxième Etoile de Ski',
+    'Lieux'=>'Carpates',
+    'Date'=> '2000'
+    ],
+];
+?>
 <div class="formation">
-            <!-- partie de julien -->
+<h3>Formation</h3>
+<?php
+foreach ($studies as $key => $study) {
+    foreach ($study as $key => $value) {
+        switch ($key) {
+            case 'Diplome' === $key:
+                echo '<i>'.$value.'</i>'.'</br>';
 
-            <!-- partie end julien -->
+                break;
 
-        </div>
-        
+            case 'Date' === $key:
+                echo '<time>'.$value.'</time>'.'</br>';
+
+                break;
+
+            case 'Lieux' === $key:
+                echo '<mark>'.$value.'</mark>'.'</br>';
+
+                break;
+        }
+    }
+}
+?>
+</div>
+
