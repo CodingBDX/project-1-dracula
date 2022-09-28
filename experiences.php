28e654bb310a3e7d4d7fbe0cabcfad60fc62a7b5
CodingBDX <antonycharier@gmail.com>
2022-09-28 09:27:41 +0000

    construction php de l experiences avec un tableau

--- experiences.php
+++ experiences.php
@@ -1,6 +1,53 @@
+<?php
 
-        <div class="experiences">
-        <!-- partie de antony -->
+$experiences = [
+    [
+        'name' => 'poissonerie',
+        'address' => 'kil 33 dead',
+        'date' => '08/1300',
+        'description' => 'loremdddd dfdf df',
+    ],
+    [
+        'name' => 'bouro',
+        'address' => 'place notre dame',
+        'date' => '06.1789',
+        'description' => 'decapiter les rois et reines',
+    ],
+    [
+        'name' => '',
+        'address' => '',
+        'date' => '',
+        'description' => '',
+    ],
+];
+?>
+<div class="experiences">
+<h3>Experiences</h3>
+<?php
+foreach ($experiences as $key => $experience) {
+    foreach ($experience as $key => $value) {
+        switch ($key) {
+            case 'address' === $key:
+                echo '<i>'.$value.'</i>'."\n";
 
-        <!-- partie end antony -->
-    </div>
+                break;
+
+            case 'date' === $key:
+                echo '<time>'.$value.'</time>'."\n";
+
+                break;
+
+            case 'description' === $key:
+                echo '<mark>'.$value.'</mark>'."\n";
+
+                break;
+
+            default:'name' === $key;
+                echo '<h4>'.$value.'<h4>'."\n";
+
+                break;
+        }
+    }
+}
+?>
+</div>
