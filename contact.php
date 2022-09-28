0bdf9e02ee6067141c537ca78917cfceee42c37c
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-09-28 16:42:57 +0200

    create contacts.css and make contact css

--- contact.php
+++ contact.php
@@ -11,13 +11,17 @@
     ?>
     <h3>Contacts</h3>
     <address>
-        <h4>Adresse :</h4>
-        <p><?php echo $contacts["adresse"] ?></p>
-        <h4>Téléphone :</h4>
-        <a href="tel:+408591897"><?php echo $contacts["téléphone"] ?></a>
-        <h4>Ch@uve-souris électronique :</h4>
-        <a href="mailto:dracula@bramstoker.ro"><?php echo $contacts["courriel"] ?></a>
-        <h4>Groupe sanguin :</h4>
-        <p><?php echo $contacts["groupe-sanguin"] ?></p>
+        <div class="block location">
+            <h4>Adresse :</h4>
+            <p><?php echo $contacts["adresse"] ?></p>
+        </div>
+        <div class="block">
+            <h4>Téléphone :</h4>
+            <a href="tel:+408591897"><?php echo $contacts["téléphone"] ?></a>
+            <h4>Ch@uve-souris électronique :</h4>
+            <a href="mailto:dracula@bramstoker.ro"><?php echo $contacts["courriel"] ?></a>
+            <h4>Groupe sanguin :</h4>
+            <p><?php echo $contacts["groupe-sanguin"] ?></p>
+        </div>
     </address>
 </div>
