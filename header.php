fdc60c3fdbcb1b085fbb41b591acfaff2f252177
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-09-28 14:58:13 +0200

    finish header (html/css), mobile version

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <header>
-    <div id="banner">
-        <img id="photo-profil" src="" alt="photo-profil" srcset="">
+    <div class="banner">
+        <img class="photo-profil" src="img/dracula.jpg" alt="photo-profil" srcset="">
         <h1>Dracula</h1>
     </div>
 
-    <h2>Médecin de prélèvement mobile pour la banque de sang</h2>
+    <h2>Technicien de prélèvement pour la banque de sang</h2>
 </header>
