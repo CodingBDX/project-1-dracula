2e3f64c2717c3ff7f257090a8640b0ada130c42b
NaomieAtil <113367031+NaomieAtil@users.noreply.github.com>
2022-09-30 10:11:01 +0200

    update hobbies.php

--- hobbies.php
+++ hobbies.php
@@ -1,7 +1,54 @@
-    <div class="hobbies">
-            <!-- partie de naomie -->
-            <!-- partie end naomie -->
+    <?php
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="hobbies" href="hobbies.css"/>
+</head>
+<body>
+<div classe="titlehobbies"> 
+   <strong> <h3> Hobbies </h3> </strong>
+    </div>
 
+    <div classe="contenthobbies">
+    <ul>
+    <h4> <li> Hypnose </li></h4>
+    <h4> <li> Consumption of human blood </li></h4>
+    <h4> <li> Run away from sunlight </li></h4>
+    <h4> <li> Transform into a bat </li></h4>
+    <h4> <li> Playing piano </li></h4>
+    <h4> <li> Sleeping in a coffin </li></h4>
+</ul>
+    </div>
 
-        </div>
+</body>
+</html>
+
+body {
+    background-color: var(--background);
+    margin: 0px;
+    padding: 0px;
+    color: var(--text);
+
+}
+.titlehobbies {
+    display: list-item;
+    font-weight: bold;
+    font-family: var (--typo-title);
+    align-items: center;
+    justify-content: center;
+    background-color: var(--background-els);
     
+    
+}
+
+.contenthobbies {
+    color: var(--text);
+    display: block;
+    font-family: var(--typo-title);
+    font-size: 2.2em;
+    text-align: start;
+    ?>
+<PHP> echo '<link rel="stylesheet" type="text/css" href="hobbies.css"></head>'; <PHP>
