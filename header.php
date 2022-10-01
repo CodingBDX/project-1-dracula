84f67d3b4a3626820711e9f3657a5ab3cf2b1d37
CodingBDX <antonycharier@gmail.com>
2022-10-01 16:51:16 +0000

    toggle+langue

--- header.php
+++ header.php
@@ -1,8 +1,22 @@
 <header>
     <div class="banner">
+
+
         <img class="photo-profil" src="img/dracula.png" alt="photo-profil" srcset="">
         <h1>Dracula</h1>
     </div>
+<div class="button-toggle">
+    <div>
+    <label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label>
+</div>
+<div>
+    <button>FR</button>
+    <button>EN</button>
 
-    <h2>Technicien de prélèvement pour la banque de sang</h2>
+</div>
+</div>
 </header>
+    <h2>Technicien de prélèvement pour la banque de sang</h2>
