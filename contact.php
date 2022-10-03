f3b5487902c6f44f640b62d83f473f8f3a9d5b0e
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-10-03 16:45:03 +0200

    add icons in header

--- contact.php
+++ contact.php
@@ -12,15 +12,15 @@
     <h3>Contacts</h3>
     <address>
         <div class="block">
-            <h4>Adresse :</h4>
+            <h4><img src="img/castle.png" alt="castle-icon"> Adresse :</h4>
             <p><?php echo $contacts["adresse"] ?></p>
         </div>
         <div class="block">
-            <h4>Téléphone :</h4>
+            <h4><img src="img/phone-call.png" alt="phone-icon"> Téléphone :</h4>
             <a href="tel:+408591897"><?php echo $contacts["téléphone"] ?></a>
-            <h4>Ch@uve-souris électronique :</h4>
+            <h4><img src="img/bat.png" alt="bat-icon"> Ch@uve-souris électronique :</h4>
             <a href="mailto:dracula@bramstoker.ro"><?php echo $contacts["courriel"] ?></a>
-            <h4>Groupe sanguin :</h4>
+            <h4><img src="img/blood-donation.png" alt="blood-icon"> Groupe sanguin :</h4>
             <p><?php echo $contacts["groupe-sanguin"] ?></p>
         </div>
     </address>
