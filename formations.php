d9f13dc507a7defc33ac6bed42800036eaa702ef
BONZOM Julien <bonzomj@gmail.com>
2022-10-03 14:54:45 +0200

    Creation Branche Formation

--- formations.php
+++ formations.php
@@ -1,56 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    
+    
+    <title>Document</title>
+
 <?php
 $studies = [
     [
     'Diplome'=>'CAP petite enfance',
     'Lieux'=>'Timiçoara', 
-    'Date'=> '1623'
+    'Date_debut'=> '1600',
+    'Date_fin'=> '-1632',
     ],
     [
     'Diplome'=>'Baccalaureat série ES', 
     'Lieux'=>'Bucarest', 
-    'Date'=> '1897',
+    'Date_debut'=> '1800',
+    'Date_fin'=> '-1836',
     ],
     [
     'Diplome'=>'BTS  Hostellerie restauration',
     'Lieux'=>'Transilvanie',
-    'Date'=> '1900',
+    'Date_debut'=> '1898',
+    'Date_fin'=> '-1900',
     ],
     [
     'Diplome'=>'Doctora de Medecin légiste',
     'Lieux'=>'Paris',
-    'Date'=> '1940'
+    'Date_debut'=> '1900',
+    'Date_fin'=> '-1940',
     ],
     [
     'Diplome'=>'Deuxième Etoile de Ski',
     'Lieux'=>'Carpates',
-    'Date'=> '2000'
+    'Date_debut'=> '2000',
+    'Date_fin'=> '-2022',
     ],
 ];
 
 ?>
 <div class="formation">
 <h3>Formation</h3>
+<span class="drop"></span><span class="drop"></span><span class="drop"></span><span class="drop"></span>
+</div>
+<div class="content">
 <?php
 foreach ($studies as $key => $study) {
+    echo'<div class="centre">';
     foreach ($study as $key => $value) {
+        
+        
+        //echo "<i>".$value."</i>";
         switch ($key) {
+            
+            
+            
+            
             case 'Diplome' === $key:
-                echo '<i>'.$value.'</i>'.'</br>';
+                echo'<div class="tabulations">';
+                echo '<i>'.$value.'</i>';
+                echo'</div>';
+            break;
+                
+            
+            
+            case 'Date_debut' === $key:
+               
+                echo '<i>'.$value.'</i>';
+                
+    
+            break;
+            
+            case 'Date_fin' === $key:
+                
+                echo '<i>'.$value.'</i>';
+                
+    
+            break;
 
-                break;
-
-            case 'Date' === $key:
-                echo '<time>'.$value.'</time>'.'</br>';
-
-                break;
 
             case 'Lieux' === $key:
-                echo '<mark>'.$value.'</mark>'.'</br>';
-
-                break;
-        }
+                echo'<div class="tabulations">';               
+                echo '<i>'.$value.'</i>';
+                echo'</div>';  
+ 
+            break;
+            
+        }  
+        
+         
+        
+        
+        
+           
+    
     }
+    
+    echo '<br>'; 
+    echo'</div>';
 }
+
 ?>
 </div>
 
